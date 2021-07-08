2b350b960ca9b2ac271053d295d6f2a1b9028c05
jonrda <ruizdeazua.jon@gmail.com>
2021-07-08 09:34:43 +0200

    Lecture 05, sll search, print, delete, append completed

--- lecture_05/sll.c
+++ lecture_05/sll.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Definition of the sll node
 typedef struct node
@@ -15,12 +16,19 @@ node;
 node *create(int num);
 void print_list(node *list);
 void append(node *list, int num);
+bool search(node *list, int num);
+void delete(node *list, int num);
 
 int main(void)
 {
-    node *a = create(8);
-    append(a, 8);
-    append(a, 9);
+    node *a = create(1);
+    append(a, 2);
+    append(a, 3);
+    append(a, 4);
+    append(a, 5);
+    print_list(a);
+    delete(a, 1);
+    //delete(a, 1);
     print_list(a);
 }
 
@@ -64,3 +72,48 @@ void print_list(node *list)
     printf("%i]\n", tmp->number);
 }
 
+// Search if an element is on the list
+bool search(node *list, int num)
+{
+    node *tmp = list;
+    while (tmp != NULL)
+    {
+        if (tmp->number == num)
+        {
+            return true;
+        }
+        tmp = tmp->next;
+    }
+    return false;
+}
+
+// Delete element from list, if contained
+void delete(node *list, int num)
+{
+    node *tmp = list;
+    node *tmp0 = NULL;      // Previous node
+    while (tmp != NULL)
+    {
+        printf("Current element: %i\n", tmp->number);
+        if (tmp->number == num)
+        {
+            // If deleting node is the first
+            if (tmp0 == NULL)
+            {
+                // Won't work, list is a copy of pointer list
+                list = list->next;
+                printf("Inner list: ");
+                print_list(list);
+            }
+            else
+            {
+                tmp0->next = tmp->next;
+                free(tmp);
+            }
+            return;
+        }
+        // Edge case is when tmp0 is null
+        tmp0 = tmp;
+        tmp = tmp->next;
+    }
+}
